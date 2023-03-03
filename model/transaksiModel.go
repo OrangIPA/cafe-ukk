72b4511395c962a4b5011f5968cb327782048b6e
Bahana Alana Khansa <alanabahana102@gmail.com>
2023-03-03 23:25:36 +0700

    removed some more tags

--- model/transaksiModel.go
+++ model/transaksiModel.go
@@ -5,8 +5,8 @@ import "time"
 type Transaksi struct {
 	ID int `gorm:"primaryKey; autoIncrement"`
 	TglTransaksi time.Time `gorm:"not null"`
-	User User `gorm:"not null; foreignKey"`
-	Meja Meja `gorm:"foreignKey"`
+	User User `gorm:"not null"`
+	Meja Meja
 	NamaPelanggan string
 	Status StatusTransaksi `sql:"type:ENUM('BELUM_BAYAR','LUNAS')" gorm:"not null"`
 }
