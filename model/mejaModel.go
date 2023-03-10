5fb6b41d43d4e56cf6eca08f25340aa1e7c82695
Bahana Alana Khansa <alanabahana102@gmail.com>
2023-03-10 19:39:01 +0700

    added status column on table meja

--- model/mejaModel.go
+++ model/mejaModel.go
@@ -3,5 +3,6 @@ package model
 type Meja struct {
 	MejaID    uint   `json:"mejaId" gorm:"primaryKey; autoIncrement"`
 	NomorMeja string `json:"nomorMeja" gorm:"not null"`
+	Status    string `json:"status" gorm:"type:enum('kosong', 'terisi'); default: kosong; not null"`
 	Transaksi []Transaksi
 }
